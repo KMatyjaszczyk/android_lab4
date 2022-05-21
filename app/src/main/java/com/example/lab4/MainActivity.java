992d3fab8fc27ccf243a249b4919e14d21162be3
Krzysztof Matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-05-21 23:14:09 +0200

    Create layout, connect elements with fields

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mTextUrl;
+    private Button mButtonReceiveInformation;
+    private TextView mFileSizeValue;
+    private TextView mFileTypeValue;
+    private Button mButtonDownloadFile;
+    private TextView mBytesDownloadedValue;
+    private ProgressBar mBytesDownloadedProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectLayoutElementsWithFields();
+    }
+
+    private void connectLayoutElementsWithFields() {
+        mTextUrl = findViewById(R.id.editTextUrl);
+        mButtonReceiveInformation = findViewById(R.id.buttonReceiveInformation);
+        mFileSizeValue = findViewById(R.id.textViewFileSizeValue);
+        mFileTypeValue = findViewById(R.id.textViewFileTypeValue);
+        mButtonDownloadFile = findViewById(R.id.buttonDownloadFile);
+        mBytesDownloadedValue = findViewById(R.id.textViewBytesDownloadedValue);
+        mBytesDownloadedProgress = findViewById(R.id.progressBarBytesDownloaded);
     }
 }
